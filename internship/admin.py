24838c829836335efafd687024c283d5490f11ca
moriso-m <injectsql001.gmail.com>
2019-06-16 01:34:31 +0300

    added admin.py

---
+++ internship/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Department, Course, Student, Coordinator,Supervisor, Application, Organization, Message, Logbook
+
+# register models
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Coordinator)
+admin.site.register(Supervisor)
+admin.site.register(Application)
+admin.site.register(Organization)
+admin.site.register(Message)
+admin.site.register(Logbook)
